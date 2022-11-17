9cd11e1f0a93aaa394d30cdea674b823516600a9
Tim Carrington <timiscarrington@gmail.com>
2022-11-17 11:00:58 -0800

    reformatting buildDeck function, and creating new global variables for values, and suits

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const hitBtn = document.querySelector('#hit');
 const standBtn = document.querySelector('#stand')
 
 
-const deck = []
+ 
   
+let cards={
+    deck : [],
+    suits: [],
+    values: []
+  }
 const playerOne = {
     sumCards: 0,
     cards: [''],
@@ -27,25 +32,31 @@ const startGame = () => {
 const buildDeck = () => {
     let values = ['A', '2', '3', '4', '5', '6', '7','8','9','10','J','Q', 'K']
     let suits = ['C','D','H','S']
-    let newDeck = deck
 
     for(let i=0; i < suits.length; i++ ){
         for(j=0; j< values.length; j++) {
-            deck.push(values[j] + suits[i])
+            cards.deck.push(values[j] + suits[i])
+        }let cardValues = parseInt(values[j]);
+         if(values[j]== 'K' || values[j] == 'Q' || values[j]== 'J'){
+                cardValues = 10
+        }if(values[j] == 'A'){
+            cardValues = 11
+            cards.suits.push( suits[i])
+            cards.values.push(cardValues)
         }
     }
-    return deck;
+    return cards;
 }
 
 const shuffle = () => {
 
-    for(let i = deck.length -1; i >0; i--){ // interates through array of cards in a descending order from 51-1
+    for(let i = cards.deck.length -1; i >0; i--){ // interates through array of cards in a descending order from 51-1
       const j = Math.floor(Math.random() * (i +1)) //gives a random number between 1-52
-      let randoIndex = deck[j]; //takes the random number and gets the element in the array at the random numbers position 
-      deck[j] = deck[i]; //the random number index position is now placed in the index of [i] (first one would be at 51)
-     deck[i] = randoIndex //sets the card that is iterated in the loop to the place of the random card, and continues to run through the loop 
+      let randoIndex = cards.deck[j]; //takes the random number and gets the element in the array at the random numbers position 
+      cards.deck[j] = cards.deck[i]; //the random number index position is now placed in the index of [i] (first one would be at 51)
+     cards.deck[i] = randoIndex //sets the card that is iterated in the loop to the place of the random card, and continues to run through the loop 
      }
-     return deck;
+     return cards.deck;
    }
 
  

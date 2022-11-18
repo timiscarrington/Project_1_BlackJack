94604a4984f9bd2876e1f2fb40dfc9dec38add9d
Tim Carrington <timiscarrington@gmail.com>
2022-11-17 21:38:06 -0800

    added pictures of cards to render in DOM

--- main.js
+++ main.js
@@ -7,16 +7,15 @@ const standBtn = document.querySelector('#stand')
   
 let cards={
     deck : [],
-    suits: ['Clubs','Diamonds','Hearts','Spades'],
+    suits: ['C','D','H','S'],
     values: ['A', '2', '3', '4', '5', '6', '7','8','9','10','J','Q', 'K'],
     
   }
 const playerOne = {
     sumCards: 0,
     cards:[],
-    
-   
 }
+
 const dealer = {
     sumCards: 0,
     cards: [],
@@ -34,8 +33,8 @@ const startGame = () => {
 
 const buildDeck = () => {
     
-    for(let i=0; i < cards.suits.length; i++ ){
-        for(j=0; j< cards.values.length; j++) {
+    for (let i = 0; i < cards.suits.length; i++ ){
+        for (j = 0; j < cards.values.length; j++) {
             cards.deck.push(cards.values[j] + '-' + cards.suits[i])
         }
     }return cards.deck
@@ -44,7 +43,7 @@ const buildDeck = () => {
 
 const shuffle = () => {
 
-    for(let i = cards.deck.length -1; i >0; i--){ // interates through array of cards in a descending order from 51-1
+    for(let i = cards.deck.length -1; i > 0; i--) { // interates through array of cards in a descending order from 51-1
       const j = Math.floor(Math.random() * (i +1)) //gives a random number between 1-52
       let randoIndex = cards.deck[j]; //takes the random number and gets the element in the array at the random numbers position 
       cards.deck[j] = cards.deck[i]; //the random number index position is now placed in the index of [i] (first one would be at 51)
@@ -53,51 +52,57 @@ const shuffle = () => {
      return cards.deck;
    }
 
-   const renderDeck = () =>{
-    document.getElementById("deck").innerHTML="";
 
-    for(let i=0; i<cards.deck.length; i++){
-    let card = document.createElement('div');
-    let value = document.createElement('div');
-    let suit = document.createElement('div');
-    card.className = 'card';
-    value.className = 'value';
-    suit.className = 'suit' + cards.deck[i].suits
-    }
-   }
  
- const deal = () => {
-   playerOne.cards= [cards.deck.shift(), cards.deck.shift()]
-   dealer.cards = [cards.deck.shift(),cards.deck.shift()]
- }
+const deal = () => {
+   playerOne.cards = [cards.deck.shift(), cards.deck.shift()]
+   dealer.cards = [cards.deck.shift(), cards.deck.shift()]
+}
 
- const displayPlayerHands = () => {
+const displayPlayerHands = () => {
     document.querySelector('#player_hand').textContent = playerOne.cards;
     document.querySelector('#dealer_hand').textContent = dealer.cards;
     document.querySelector('#hand_total').textContent = "Hand Total:" + playerOne.sumCards;
     document.querySelector('#dealer_total').textContent = "Dealer Total:" + dealer.sumCards;
- }
-
-
-const playerStand = () =>{
-    while(dealer.sumCards < 17){
-    dealer.cards.push(cards.deck.shift())
+    
+    for (let i = 0; i <= playerOne.cards.length - 1; i++) {
+        const cardValue = playerOne.cards[i].split('-');
+        let img = document.createElement("img");
+        img.src = `./cards/${cardValue[0]}-${cardValue[1]}.png`;
+        let src = document.getElementById("player_hand");
+        src.appendChild(img);
     }
-    sumOfDealer();
-    displayPlayerHands();
-
-    if(dealer.sumCards >= 17){
-        sumOfDealer();
-        displayPlayerHands();
-        endOfHand()
+    for (let i = 0; i <= dealer.cards.length - 1; i++) {
+        const cardValue = dealer.cards[i].split('-');
+        let img = document.createElement("img");
+        img.src = `./cards/${cardValue[0]}-${cardValue[1]}.png`;
+        let src = document.getElementById("dealer_hand");
+        src.appendChild(img);
     }
 }
+
+// const playerStand = () =>{
+//     while(dealer.sumCards < 17){
+//         dealer.cards.push(cards.deck.shift())
+        
+        
+        
+//         sumOfDealer();
+//         displayPlayerHands();
+
+//         if(dealer.sumCards >= 17){
+//             sumOfDealer();
+//             displayPlayerHands();
+//             endOfHand()
+//             }
+//         }
+//     }
  const playerHit = () => {
+    playerOne.cards.push(cards.deck.shift()) 
     
- playerOne.cards.push(cards.deck.shift()) 
- sumOfHand();
- bust();
- displayPlayerHands();
+    sumOfHand();
+    bust();
+    displayPlayerHands();
 }
 
 const endOfHand = ()=>{
@@ -114,43 +119,44 @@ const endOfHand = ()=>{
 const sumOfHand = () => {
     let cardInfo = playerOne.cards
     let sum = 0
-    for(i = 0; i<cardInfo.length; i++){
+        for(let i = 0; i<cardInfo.length; i++){
     if(cardInfo[i].includes('K')|| cardInfo[i].includes('Q')|| cardInfo[i].includes('J')){
         sum += 10;
-    }else if (cardInfo[i].includes('A')){
+            }else if (cardInfo[i].includes('A')){
         sum += 11;
-        }else{
+            }else{
         sum += parseInt(cardInfo[i])
     
         }
             playerOne.sumCards = sum
     }   
-        return playerOne.sumCards;
+    return playerOne.sumCards;
 }
+
 
 const sumOfDealer = () => {
     let cardInfo = dealer.cards
     let sum = 0
-    for(i = 0; i<cardInfo.length; i++){
-    if(cardInfo[i].includes('K')|| cardInfo[i].includes('Q')|| cardInfo[i].includes('J')){
-        sum += 10;
-    }else if (cardInfo[i].includes('A')){
-        sum += 11;
-        }else{
-        sum += parseInt(cardInfo[i])
+    for (let i = 0; i<cardInfo.length; i++){
+        if(cardInfo[i].includes('K')|| cardInfo[i].includes('Q')|| cardInfo[i].includes('J')){
+            sum += 10;
+        } else if (cardInfo[i].includes('A')) {
+            sum += 11;
+        } else {
+            sum += parseInt(cardInfo[i])
         }
-            dealer.sumCards = sum
+        dealer.sumCards = sum
     }
-        return dealer.sumCards;
+    return dealer.sumCards;
 }
 
 
  
 const bust = () =>{
-    if(playerOne.sumCards > 21){
+    if (playerOne.sumCards > 21) {
         alert('You Busted')
         return true;
-     }
+    }
     return false;
 }
 
